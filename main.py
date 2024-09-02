50491520d2288b6d9cb7fccc087cd33fc751f62e
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2024-09-02 11:33:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import random
 import time
 
 st.logo(
-    "media/icons8-robot-64.png",
+    "media/icons8-robot-96.png",
     link="https://capxax.streamlit.app/",
-    icon_image="media/icons8-robot-100.png",
+    icon_image="media/icons8-robot-48.png",
 )
 
 st.set_page_config(
     page_title="Capxax",
-    page_icon="media/icons8-robot-100.png",
+    page_icon="media/icons8-robot-48.png",
 )
 
 # Define the characters to generate captcha text
