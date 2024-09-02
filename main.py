62857ddaff3f3d29c4cbc8a777707fb81b005164
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2024-09-02 11:28:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import random
 import time
 
 st.logo(
-    "media/icons8-robot-96.png",
+    "media/icons8-robot-64.png",
     link="https://capxax.streamlit.app/",
-    icon_image="media/icons8-robot-48.png",
+    icon_image="media/icons8-robot-64.png",
 )
 
 st.set_page_config(
